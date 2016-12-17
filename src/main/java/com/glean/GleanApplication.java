e6b352550d053cc66a12f15886587499a7153bfc
michaelplott <mike.plott80@gmail.com>
2016-12-17 18:30:51 -0500

    Initial push

---
+++ src/main/java/com/glean/GleanApplication.java
@@ -0,0 +1,12 @@
+package com.glean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GleanApplication.class, args);
+	}
+}
